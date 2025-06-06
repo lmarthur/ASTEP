357eb76b85303864646574e468363498e29ffd93
lmarthur <lmarthur@mit.edu>
2025-06-06 15:04:27 -0700

    Outlined pixel mask function

--- src/calibration.py
+++ src/calibration.py
@@ -27,3 +27,25 @@ def image_combine(images, method='average', sigma_clip=True, sigma=3.0):
     combined_image = ccdp.combine(images, method=method, sigma_clip=sigma_clip, sigma_clip_high_thresh=sigma, sigma_clip_low_thresh=sigma, mem_limit=mem_limit)
     return combined_image
 
+def generate_mask(flat_images, path=None):
+    """
+    Generate a pixel mask based on the set of flat images, using the ccdproc.mask_bad_pixels function.
+
+    INPUTS:
+    ------------------
+    flat_images: ccdproc.ImageFileCollection
+        A list of calibrated, uncombined flat field images to analyze for pixel masking.
+
+    path: str, optional
+        The file path where the mask will be saved. If None, the mask will not be saved.
+
+    OUTPUTS:
+    ------------------
+    mask: CCDData
+        A mask where bad pixels are marked as 1 and good pixels as 0.
+    
+    """
+
+    # If there are flat images with suitably different pixel values (different exposure times or different sky brightnesses), and the exposure times are long enough that read noise is not dominant, use a ratio of the images to generate a mask. 
+
+    # If the flat images are all very similar, use one calibrated (but not combined) flat to generate a mask
